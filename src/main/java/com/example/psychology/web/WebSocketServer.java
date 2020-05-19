cd4af1587cf2f15dbfd8abab96aeec66d2c4b280
yangwei <vit125@163.com>
2020-05-19 11:49:41 +0800

    final1

--- src/main/java/com/example/psychology/web/WebSocketServer.java
+++ src/main/java/com/example/psychology/web/WebSocketServer.java
@@ -1,5 +1,7 @@
 package com.example.psychology.web;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.OnClose;
@@ -15,31 +17,48 @@ import java.util.concurrent.ConcurrentHashMap;
 @ServerEndpoint("/websocket/{id}")
 public class WebSocketServer {
 
-    private static Map<String, WebSocketServer> clients = new ConcurrentHashMap<>();
-
-    private Session session;
+    private static Map<String, Session> sessionMap = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(@PathParam("id") String id, Session session) {
-        this.session = session;
-        clients.put(id, this);
-        System.out.println(id + "连接了");
+    public void onOpen(@PathParam("id") String path, Session session) {
+        String[] split = path.split(":");
+        String fromId = split[0];
+        String toId = split[1];
+        sessionMap.put(fromId, session);
+        if (sessionMap.get(toId) != null) {
+            sessionMap.get(toId).getAsyncRemote().sendText("系统提示：对方上线了");
+            sessionMap.get(fromId).getAsyncRemote().sendText("系统提示：对方已在线");
+        } else {
+            sessionMap.get(fromId).getAsyncRemote().sendText("系统提示：等待对方上线");
+
+        }
     }
 
     @OnMessage
-    public void onMessage(@PathParam("id") String id, String data) {
-        String[] split = data.split(":");
-        System.out.println(split[0]);
-        System.out.println(split[1]);
-        Session session = clients.get(split[0]).session;
-        if (session != null) {
-            session.getAsyncRemote().sendText(split[1]);
+    public void onMessage(String data) {
+        JSONObject jsonObject = JSON.parseObject(data);
+        String fromName = jsonObject.getString("fromName");
+        String toName = jsonObject.getString("toName");
+        String fromId = jsonObject.getString("fromId");
+        String toId = jsonObject.getString("toId");
+        String text = jsonObject.getString("text");
+        if (sessionMap.get(toId) != null) {
+            sessionMap.get(fromId).getAsyncRemote().sendText(fromName + "：" + text);
+            sessionMap.get(toId).getAsyncRemote().sendText(fromName + "：" + text);
+        } else {
+            sessionMap.get(fromId).getAsyncRemote().sendText("系统提示：发送失败，" + toName + "对方不在线");
         }
     }
 
     @OnClose
-    public void onClose(@PathParam("id") String id) {
-        System.out.println(id + "关闭了");
+    public void onClose(@PathParam("id") String path) {
+        String[] split = path.split(":");
+        String fromId = split[0];
+        String toId = split[1];
+        sessionMap.remove(fromId);
+        if (sessionMap.get(toId) != null) {
+            sessionMap.get(toId).getAsyncRemote().sendText("系统提示：对方掉线");
+        }
     }
 
 }
